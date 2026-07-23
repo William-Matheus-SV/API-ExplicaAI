8218ad446a4c96a6cac3a89349b4075dba4918be
William Matheus <william.matheus7765@gmail.com>
2026-07-22 21:34:03 -0300

    subindo a parte de Luis

---
+++ src/routes/usuarioRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+const usuarioController = require("../controllers/usuarioController");
+
+router.post("/usuarios", usuarioController.cadastrarUsuario);
+
+module.exports = router;
