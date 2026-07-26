f9e57bd5886d7d163067c6032683f83a9e63e635
William Matheus <william.matheus7765@gmail.com>
2026-07-26 13:42:57 -0300

    Add novos métodos no Usuario.js e add rotas de acesso local e nuvem ao MongoDB e Atlas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-//const authRoutes = require('./routes/authRoutes');
-//const usuarioRoutes = require('./routes/usuarioRoutes');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
@@ -9,8 +9,18 @@ const app = express();
 app.use(cors()); // Permite que o React (Front-end) acesse essa API
 app.use(express.json()); // Habilita o Express a ler corpos de requisições em formato JSON
 
+//Conexão com o MongoDB Atlas
+//const MONGO_URI = "mongodb+srv://williammatheus7765_db_user:tmL1HRIlYqZ4ZooN@explicaai.zkdlvx7.mongodb.net/?appName=ExplicaAi";
+
+//Conexão com MongoDB Local
+const MONGO_URI = "mongodb://localhost:27017/explicaai";
+
+mongoose.connect(MONGO_URI)
+    .then(() => console.log("Conectado ao MongoDB Atlas com sucesso!"))
+    .catch((err) => console.error("Erro ao conectar no MongoDB Atlas:", err));
+
 //Importando as rotas de cada branch
-const usarioRoutes = require('./routes/usuarioRoutes');
+const usuarioRoutes = require('./routes/usuarioRoutes');
 const authRoutes = require('./routes/authRoutes');
 
 // Vinculando as rotas aos caminhos da API
@@ -19,7 +29,7 @@ app.use('/api', usuarioRoutes);
 
 // Rota de teste inicial para verificar se a API está online
 app.get('/api/teste', (req, res) => {
-    res.json({ mensagem: "API do ExplicaAí funcionando e aguardando conexões! 🚀" });
+    res.json({ mensagem: "API do ExplicaAí funcionando e aguardando conexões!" });
 });
 
 // Configuração da porta
--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -4,39 +4,51 @@ const usuarioSchema = new mongoose.Schema({
 
     nome: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
 
     matricula: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        trim: true
     },
 
     idade: {
         type: Number,
-        required: true
+        required: [true, "A idade é obrigatória"],
+        min: [14, "Idade mínima permitida é 14 anos"],
+        max: [100, "Idade máxima permitida é 100 anos"]
     },
 
     materias: [{
-        type: String
+        type: String,
+        trim: true
     }],
 
     bio: {
-        type: String
+        type: String,
+        trim: true,
+        maxlength: [500, "A biografia não pode passar de 500 caracteres"]
     },
 
     email: {
         type: String,
-        required: true,
-        unique: true
+        required: [true, "O e-mail é obrigatório"],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [/\S+@\S+\.\S+/, "Por favor, insira um e-mail válido"]
     },
 
     senha: {
         type: String,
-        required: true
+        required: [true , "A senha é obrigatória"],
+        minlength: [6, "A senha deve ter no mínimo 6 caracteres"]
     }
-
+},{
+    timestamps: true
 });
 
 module.exports = mongoose.model("Usuario", usuarioSchema);
