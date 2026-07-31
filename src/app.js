a3d17d802b0cf5f5dd03e750a9f4255625e4708a
William Matheus <william.matheus7765@gmail.com>
2026-07-31 19:03:08 -0300

    Att's e novos arquivos match add

--- src/app.js
+++ src/app.js
@@ -6,30 +6,38 @@ const mongoose = require('mongoose');
 
 
 const app = express();
-conectarBanco(); // importei a conexão
 
 // Middlewares Globais vitais para o projeto
-app.use(cors()); // Permite que o React (Front-end) acesse essa API
+app.use(cors({
+    origin: process.env.FRONTEND_URL || '*',
+    credentials: true
+})); // Permite que o React (Front-end) acesse essa API
+
 app.use(express.json()); // Habilita o Express a ler corpos de requisições em formato JSON
 
+app.use(express.urlencoded({extended: true})); //Permite ler dados de formulário
+
 //Conexão com o MongoDB Atlas
-//const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://williammatheus7765_db_user:tmL1HRIlYqZ4ZooN@explicaai.zkdlvx7.mongodb.net/?appName=ExplicaAi";
 
 //Conexão com MongoDB Local
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/explicaai";
 
 mongoose.connect(MONGO_URI)
-    .then(() => console.log("Conectado ao MongoDB Atlas com sucesso!"))
-    .catch((err) => console.error("Erro ao conectar no MongoDB Atlas:", err));
+    .then(() => console.log("Conectado ao MongoDB com sucesso!"))
+    .catch((err) => console.error("Erro ao conectar no MongoDB :", err));
 
 //Importando as rotas de cada branch
 const usuarioRoutes = require('./routes/usuarioRoutes');
 const authRoutes = require('./routes/authRoutes');
+const tutorRoutes = require('./routes/tutorRoutes');
+//const matchRoutes = require('./routes/matchRoutes'); // Criar depois
 
 // Vinculando as rotas aos caminhos da API
 app.use('/api/auth', authRoutes);
 app.use('/api', usuarioRoutes);
 app.use("/api", tutorRoutes);
+//app.use('/api/matches', matchRoutes);  // Matches (se houver)
+
 
 // Rota de teste inicial para verificar se a API está online
 app.get('/api/teste', (req, res) => {
--- src/config/db.js
+++ src/config/db.js
@@ -5,9 +5,9 @@ const conectarBanco = async () => {
     try {
         await mongoose.connect("mongodb://localhost:27017/");
 
-        console.log("✅ Banco conectado com sucesso!");
+        console.log(" Banco conectado com sucesso!");
     } catch (erro) {
-        console.error("❌ Erro ao conectar no MongoDB");
+        console.error(" Erro ao conectar no MongoDB");
         console.error(erro.message);
         process.exit(1);
     }
