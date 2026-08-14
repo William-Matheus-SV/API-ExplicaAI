56270f4ae1b460a0ac5f691fee25d35e7ea875cb
William Matheus <william.matheus7765@gmail.com>
2026-08-13 21:51:08 -0300

    add validação de Login na rota de autenticação

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
-router.post("/login/aluno", authController.loginUsuario); // Rota POST para login de usuário
-router.post("/login/tutor", authController.loginTutor); // Rota POST para login de tutor
+router.post("/login/aluno", validarLogin, authController.loginUsuario); // Rota POST para login de usuário
+router.post("/login/tutor", validarLogin, authController.loginTutor); // Rota POST para login de tutor
 
 module.exports = router;
