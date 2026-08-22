7fc68de3ec41d976ff8a63f3c6ef127288250b75
Luis Henrique <dasilvapereiraluishenrique@gmail.com>
2026-08-22 16:35:59 -0300

    mudei o corpo do db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,18 @@
-// add banco local
+require('dotenv').config();
 const mongoose = require("mongoose");
 
 const conectarBanco = async () => {
-    try {
-        await mongoose.connect("mongodb://localhost:27017/");
+    if (!process.env.MONGO_URI) {
+        console.error("Erro: variável MONGO_URI não encontrada no ambiente.");
+        process.exit(1);
+    }
 
-        console.log(" Banco conectado com sucesso!");
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+
+        console.log("Banco conectado com sucesso!");
     } catch (erro) {
-        console.error(" Erro ao conectar no MongoDB");
+        console.error("Erro ao conectar no MongoDB");
         console.error(erro.message);
         process.exit(1);
     }
