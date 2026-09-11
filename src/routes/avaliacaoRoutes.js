07edf67bc711f213cf0b10638b590ec5efc62d9e
William Matheus <william.matheus7765@gmail.com>
2026-09-10 21:04:14 -0300

    att

--- src/routes/avaliacaoRoutes.js
+++ src/routes/avaliacaoRoutes.js
@@ -3,12 +3,10 @@ const router = express.Router();
 const avaliacaoController = require("../controllers/avaliacaoController");
 const verificarToken = require("../middlewares/verificarToken");
 
-router.get("/teste", (req, res) => {
-    res.json({ mensagem: "Rotas de avaliação estão funcionando!" });
-});
-
 router.post("/", verificarToken, avaliacaoController.criarAvaliacao);
 router.get("/minhas", verificarToken, avaliacaoController.listarMinhasAvaliacoes);
 router.get("/tutor/:tutorId", avaliacaoController.listarAvaliacoesDoTutor);
+router.get("/enviadas", verificarToken, avaliacaoController.listarEnviadas);
+router.get("/pendentes", verificarToken, avaliacaoController.listarPendentes);
 
 module.exports = router;
